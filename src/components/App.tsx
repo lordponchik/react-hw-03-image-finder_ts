e732cfd9046c5e63724a20934795889d8fa0f583
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-09-30 12:53:36 +0200

    add service PixabauAPI with func getImages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,11 +4,24 @@ import './App.css';
 import Searchbar from '../components/Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 
-class App extends Component<{}, {}> {
+import getImages from '../services/PixabauAPI';
+
+interface State {
+  images: {};
+  query: string;
+}
+
+class App extends Component<{}, State> {
   state = {
     images: [],
     query: '',
   };
+
+  componentDidUpdate(prevState: State): void {
+    if (this.state.query.length !== 0 && prevState.query !== this.state.query) {
+      getImages(this.state.query);
+    }
+  }
 
   qetQuery = (valueInput: string) => {
     this.setState({ query: valueInput });
---
+++ src/services/PixabauAPI.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const KEY = '36810234-b5e1d7960ec1148affe35137c';
+
+axios.defaults.baseURL = `https://pixabay.com/api/?`;
+
+const config = `&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+async function getImages(q: string) {
+  const response = await axios.get(`q=${q}${config}`);
+  const data = await response.data;
+
+  console.log(data.hits);
+  return data;
+}
+
+export default getImages;
