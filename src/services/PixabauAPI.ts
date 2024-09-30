d0d8e8a13a5a32cd2327899014c4da447bafe039
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-09-30 18:22:10 +0200

    change baseUrl for func getImages

--- src/services/PixabauAPI.ts
+++ src/services/PixabauAPI.ts
@@ -2,16 +2,16 @@ import axios from 'axios';
 
 const KEY = '36810234-b5e1d7960ec1148affe35137c';
 
-axios.defaults.baseURL = `https://pixabay.com/api/?`;
+axios.defaults.baseURL = `https://pixabay.com/api`;
 
 const config = `&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`;
 
 async function getImages(q: string) {
-  const response = await axios.get(`q=${q}${config}`);
+  const response = await axios.get(`?q=${q}${config}`);
   const data = await response.data;
+  const hits = await data.hits;
 
-  console.log(data.hits);
-  return data;
+  return hits;
 }
 
 export default getImages;
