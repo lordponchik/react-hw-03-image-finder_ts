ac52bfd7ea5a8e675fa4827c5a0a750c19ed6947
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-10-08 15:17:05 +0200

    add scroll for ImageGallery

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,22 +1,53 @@
 import IImage from '../../interfaces/Image.inteface';
 import style from './ImageGallery.module.css';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import React, { Component } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 
 interface Props {
   images: IImage[];
   isOpenModal: (modalData: string) => void;
 }
+interface State {}
 
-const ImageGallery = ({ images, isOpenModal }: Props) => {
-  return (
-    <ul className={style.gallery}>
-      {images.map((image: IImage, index) => {
-        return (
-          <ImageGalleryItem key={`${image.id}_${index}`} image={image} isOpenModal={isOpenModal} />
-        );
-      })}
-    </ul>
-  );
-};
+class ImageGallery extends Component<Props, State> {
+  listRef = React.createRef<HTMLUListElement>();
+
+  getSnapshotBeforeUpdate(prevProps: Props, prevState: State): number | null {
+    const list = this.listRef.current;
+    if (list) {
+      return list.scrollHeight;
+    }
+    return null; // Вернуть null, если список отсутствует
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State, snapshot: number | null) {
+    if (snapshot !== null) {
+      const list = this.listRef.current;
+
+      if (list) {
+        scroll.scrollTo(snapshot);
+      }
+    }
+  }
+
+  render() {
+    const { images, isOpenModal } = this.props;
+
+    return (
+      <ul className={style.gallery} ref={this.listRef}>
+        {images.map((image: IImage, index) => {
+          return (
+            <ImageGalleryItem
+              key={`${image.id}_${index}`}
+              image={image}
+              isOpenModal={isOpenModal}
+            />
+          );
+        })}
+      </ul>
+    );
+  }
+}
 
 export default ImageGallery;
