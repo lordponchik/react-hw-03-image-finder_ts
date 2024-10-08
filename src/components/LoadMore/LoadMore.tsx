40df94774e1f0a64c6ced25955e1fd6adea36eed
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-10-08 13:50:52 +0200

    add styles for LoadMore

--- src/components/LoadMore/LoadMore.tsx
+++ src/components/LoadMore/LoadMore.tsx
@@ -1,10 +1,12 @@
+import style from './LoadMore.module.css';
+
 interface Props {
   nextPage: () => void;
 }
 
 const LoadMore = ({ nextPage }: Props) => {
   return (
-    <button type="button" onClick={nextPage}>
+    <button type="button" onClick={nextPage} className={style.loadMore}>
       LoadMore
     </button>
   );
